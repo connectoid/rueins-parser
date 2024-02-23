7e8127b272c3d6b4aed3dae62ff90a482ed07105
Alexander Beley <beleysasha@gmail.com>
2024-02-23 15:55:24 +1200

    testing downloading

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,7 +11,15 @@ base_url = 'https://rueins.ru/'
 
 ua = UserAgent()
 
-pdf_folder = 'pdf'
+downloads_dir = '/var/www/www-root/data/www/manualbase.ru/uploads/download/electro'
+downloads_thumbs_dir = '/var/www/www-root/data/www/manualbase.ru/uploads/download/electro/thumbs'
+
+# downloads_dir = 'pdf'
+# downloads_thumbs_dir = 'pdf/thumbs'
+
+
+
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
@@ -21,7 +30,7 @@ def get_fake_headers():
 
 def get_categories_list(url):
     categories_list = []
-    response = requests.post(url=url, headers=get_fake_headers())
+    response = requests.post(url=url, headers=headers)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
     links = soup.find_all('a', class_='section-link')
@@ -34,7 +43,7 @@ def get_categories_list(url):
 
 def get_brands_list(url): 
     brands_list = []
-    response = requests.post(url=url, headers=get_fake_headers())
+    response = requests.post(url=url, headers=headers)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
     div = soup.find_all('div', class_='tabs__content')[1]
@@ -53,7 +62,7 @@ def get_brands_list(url):
 def get_models_list(url):
     model_urls = []
     model_titles = []
-    response = requests.post(url=url, headers=get_fake_headers())
+    response = requests.post(url=url, headers=headers)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
     nav_links = soup.find('div', class_='nav-links')
@@ -63,7 +72,7 @@ def get_models_list(url):
         pages_count = int(pages[-2])
         for page in range(2, pages_count + 1):
                 new_url = f'{url}page/{page}/'
-                response = requests.post(url=new_url, headers=get_fake_headers())
+                response = requests.post(url=new_url, headers=headers)
                 response.encoding = 'utf-8'
                 soup = BeautifulSoup(response.text, 'lxml')
                 divs = soup.find_all('div', class_='entry-title')
@@ -84,15 +93,19 @@ def get_models_list(url):
 
 
 def get_manual_link(url):
-    response = requests.post(url=url, headers=get_fake_headers())
+    response = requests.post(url=url, headers=headers)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
     manual_link = []
     try:
         file_link = soup.find('div', class_='main-content-iframe').find('a')['href']
+        thumb_link = soup.find('div', class_='main-content-iframe').find('img')['src']
         file_name = file_link.split('/')[-1]
+        thumb_name = thumb_link.split('/')[-1]
         manual_link.append(file_name)
         manual_link.append(file_link)
+        manual_link.append(thumb_name)
+        manual_link.append(thumb_link)
     except TypeError as e:
         print(f'Ошибка парсинга: {e}')
         return False
@@ -102,13 +115,16 @@ def get_manual_link(url):
     return manual_link
 
 
-def download_file_from_url(url, file_name, dest_folder):
+def download_file_from_url(url, file_name, dest_folder, is_thumb=False):
     response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         file_name = f"{dest_folder}/{file_name}"
         with open(file_name, mode="wb") as file:
             file.write(response.content)
         filesize = os.path.getsize(file_name)
+        if is_thumb:
+            dst = f"{dest_folder}/mini/{file_name}"
+            shutil.copyfile(file_name, dst)
         return filesize
     else:
         print(f'Ошибка сохранения файла {file_name}: {response.status_code}')
@@ -121,17 +137,22 @@ def create_xfields(cat_name='', brand_name='', lang='русском', format='pd
     return xfields
 
 
-brands = get_brands_list(base_url)
-brands = brands[0]
+all_brands = get_brands_list(base_url)
+brands = []
+brands.append(all_brands[1])
 for brand in brands:
     categories = get_categories_list(brand[1])
     for category in categories:
         models = get_models_list(category[1])
         for model in models:
             manual_link = get_manual_link(model[1])
-            filesize = download_file_from_url(manual_link[1], manual_link[0], pdf_folder)
+            filesize = download_file_from_url(manual_link[1], manual_link[0], downloads_dir)
+            thumbsize = download_file_from_url(manual_link[3], manual_link[2], downloads_thumbs_dir, is_thumb=True)
             if filesize:
                 print(f'Файл {manual_link[0]} сохранен, размер: {filesize}')
                 xfields = create_xfields(category[0], brand[0])
-                if create_download(model[0], xfields, 6, manual_link[0], filesize, ''):
+                # print(f'{manual_link[0]}, {manual_link[1]}, {manual_link[2]}, {xfields}, {filesize}')
+                if create_download(model[0], xfields, 6, manual_link[0], filesize, manual_link[3]):
                     print('Запись успешно добавлена в БД')
+            else:
+                print('Download error')
