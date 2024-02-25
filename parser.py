f74d191f984997f45510c8149d63a7275765aabb
Alexander Beley <beleysasha@gmail.com>
2024-02-26 11:49:25 +1200

    added pdf compresing

--- parser.py
+++ parser.py
@@ -20,7 +20,7 @@ downloads_thumbs_dir = '/var/www/www-root/data/www/manualbase.ru/uploads/downloa
 # downloads_dir = 'pdf'
 # downloads_thumbs_dir = 'pdf/thumbs'
 
-MAX_FILE_SIZE = 8000000
+MAX_FILE_SIZE = 5000000
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
